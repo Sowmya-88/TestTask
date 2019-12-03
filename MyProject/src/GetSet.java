96aefdef6b8ed1e84bac236eccc49a1310ecf0aa
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-03 14:41:09 +0530

    This is my test demo project

---
+++ MyProject/src/GetSet.java
@@ -0,0 +1,18 @@
+
+public class GetSet {
+String MyBankName;
+int MyBankActNum;
+public String getMyBankName() {
+	return MyBankName;
+}
+public void setMyBankName(String myBankName) {
+	MyBankName = myBankName;
+}
+public int getMyBankActNum() {
+	return MyBankActNum;
+}
+public void setMyBankActNum(int myBankActNum) {
+	MyBankActNum = myBankActNum;
+}
+
+}
